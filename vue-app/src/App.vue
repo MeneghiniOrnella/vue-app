0a09fa0a154d0a43da2cdd7e2a8e5d097e9bfc49
Ornella Meneghini <meneghini.ornella@gmail.com>
2022-07-13 11:00:07 -0300

    update blog

---
+++ vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/dog">Dog</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #041f3d;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #b14dcf;
+}
+
+nav a.router-link-exact-active {
+  color: #172fb9;
+}
+</style>
---
+++ vue-app/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non erat non dui euismod tristique nec id purus. Phasellus et orci auctor, blandit purus ornare, sagittis arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc in viverra nisl, a laoreet metus. In molestie, mauris ac aliquet suscipit, urna lectus lacinia libero, sit amet congue ipsum justo vel mi. Etiam ullamcorper pellentesque leo, porttitor tincidunt nibh elementum at. Vestibulum ornare ultrices vestibulum.</p>
+    <p>Maecenas pharetra nulla risus, in ornare felis gravida eu. Sed fermentum quam sed lacus dictum elementum. Nam eu dignissim ipsum. Pellentesque quis pulvinar augue, nec sagittis quam. Mauris ac purus condimentum, congue dolor quis, euismod nibh. Sed vel consequat nulla. Nunc porttitor, leo cursus ultrices rhoncus, velit turpis efficitur mi, sit amet malesuada sem orci non libero. Maecenas justo arcu, laoreet et consectetur ultricies, posuere elementum dui. Pellentesque sollicitudin lorem in aliquam auctor. Nam nec elementum odio, quis accumsan nisi. Etiam accumsan lacus in lacinia elementum. Duis gravida quam erat, nec pulvinar velit malesuada at.</p>
+  </div>
+</template>
+
+<style scoped>
+p{
+  text-align: justify;
+}
+</style>
